45f8214cb65f0fc571caa417d84ec8400253eb7e
isaacrlee <isaacrlee@gmail.com>
2016-12-26 15:28:17 -0800

    Version 2.0

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Website
+Working directory for my personal website.
 
-This is my website.
+Designed and coded on my MacBook Pro in Bellevue, WA
+
+Built with VS Code.
+
+Code uses [btns](http://mrmrs.io/btns/), [colors](http://clrs.cc/), [lightbox](http://lokeshdhakar.com/projects/lightbox2/), [smooth scrolling](https://css-tricks.com/snippets/jquery/smooth-scrolling/), and [typed](http://www.mattboldt.com/demos/typed-js/).
+
+Design is inspired by hundreds of other personal website and the city of Seattle.
---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(function () {
+        $(".name").typed({
+            strings: ["Hi! I'm Isaac."],
+            typeSpeed: 25
+        });
+    });
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    $('ul#projects-filter a').click(function () {
+        $('ul#projects .hidden').removeClass('hidden');
+        $('ul#projects-filter .current').removeClass('current bg-teal');
+        $(this).parent().addClass('current bg-teal');
+        var filterVal = $(this).text().toLowerCase();
+        $('ul#projects>li').each(function () {
+            if (!$(this).hasClass(filterVal)) {
+                $(this).hide('normal');
+            } else {
+                $(this).show('normal');
+            }
+        });
+        return false;
+    });
+    $('ul#education-filter a').click(function () {
+        $('ul#education-list .hidden').removeClass('hidden');
+        $('ul#education-filter .current').removeClass('current bg-purple');
+        $(this).parent().addClass('current bg-purple');
+        var filterVal = $(this).text().toLowerCase().replace(' ','-');;
+        $('ul#education-list>li').each(function () {
+            if (!$(this).hasClass(filterVal)) {
+                $(this).hide('normal');
+            } else {
+                $(this).show('normal');
+            }
+        });
+        return false;
+    });
+});
